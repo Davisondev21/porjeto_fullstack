60172877a1ca1fe473aa92027afe4ea186371250
Davisondev21 <161637119+Davisondev21@users.noreply.github.com>
2024-05-13 08:52:12 -0400

    ..

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -7,19 +7,6 @@ const fetchTasks = async () => {
   return tasks;
 };
 
-function search() {
-  let input = document.getElementById('searchbar').value;
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName('animals');
-  for(i = 0; i < x.length; i++){
-    if(!x[i].innerHTML.toLowerCase().includes(input)){
-      x[i].style.display = "none";
-    } else {
-      x[i].style.display = "list-item";
-    }
-  }
-}
-
 const addTask = async (event) => {
   event.preventDefault();
 
