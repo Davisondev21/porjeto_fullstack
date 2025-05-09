9099ea362db7e226b13a2597869cd13b05e3153a
Davisondev21 <161637119+Davisondev21@users.noreply.github.com>
2025-05-09 14:12:05 -0400

    teste
    asd

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -16,10 +16,17 @@ const addTask = async (event) => {
     method: "post",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(task),
+    
   });
 
   loadTasks();
   inputTask.value = "";
+};
+
+const formatDate = (dateUTC) => {
+  const options = { dateStyle: "short", timeStyle: "short" };
+  const date = new Date(dateUTC).toLocaleString("pt-br", options);
+  return date;
 };
 
 const deleteTask = async (id) => {
@@ -28,14 +35,12 @@ const deleteTask = async (id) => {
   });
   loadTasks();
 };
-
+  
 const updateTask = async ({ id, title, status }) => {
-
   await fetch(`${API}/tasks/${id}`, {
     method: "put",
-    headers: { 'Content-type': 'application/json' },
+    headers: { "Content-type": "application/json" },
     body: JSON.stringify({ title, status }),
-    
   });
 
   loadTasks();
@@ -73,7 +78,7 @@ const createRow = (task) => {
 
   const tr = createElement("tr");
   const tdTitle = createElement("td", title);
-  const tdCreateAt = createElement("td", created_at);
+  const tdCreateAt = createElement("td", formatDate(created_at));
   const tdStatus = createElement("td");
   const tdActions = createElement("td");
 
@@ -82,7 +87,6 @@ const createRow = (task) => {
   select.addEventListener("change", ({ target }) =>
     updateTask({ ...task, status: target.value })
   );
-
 
   const editButton = createElement(
     "button",
@@ -95,22 +99,22 @@ const createRow = (task) => {
     "<span class='material-symbols-outlined'>delete  <span>"
   );
 
-const editForm = createElement('form');
-const editInput = createElement('input');
+  const editForm = createElement("form");
+  const editInput = createElement("input");
 
-editInput.value = title
-editForm.appendChild(editInput);
+  editInput.value = title;
+  editForm.appendChild(editInput);
 
-editForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  updateTask({ id, title: editInput.value, status  });
-});
+    updateTask({ id, title: editInput.value, status });
+  });
 
-editButton.addEventListener('click', () => {
-  tdTitle.innerText = '';
-  tdTitle.appendChild(editForm);
-} )
+  editButton.addEventListener("click", () => {
+    tdTitle.innerText = "";
+    tdTitle.appendChild(editForm);
+  });
 
   editButton.classList.add("btn-action");
   deleteButton.classList.add("btn-action");
